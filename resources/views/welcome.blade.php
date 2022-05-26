31c9e5a6db3c9300c48b895a9e22ac5277e5191e
Irving Zamora <irving@gdalab.com>
2022-05-25 21:32:41 -0600

    Se modificaron colores del layout doc-template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,231 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="cleartype" content="on">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+        integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/highlight.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,500|Source+Code+Pro:300" rel="stylesheet">
+    <script>
+        hljs.initHighlightingOnLoad();
+    </script>
+    <title>Laravel</title>
 
-        <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
-        </style>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
+    <!-- Styles -->
+
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+
+    </style>
 
     <link rel="stylesheet" href="{{ mix('/css/doc-template.min.css') }}" />
-    </head>
-    <body class="antialiased">
-        Hello world
+</head>
+
+<body>
+    <div class="left-menu">
+        <div class="content-logo">
+            <!--<img  src="images/logo.png" />-->
+        </div>
+        <div class="content-menu">
+            <ul>
+                <li class="scroll-to-link active" data-target="introduccion">
+                    <a>Introducción</a>
+                </li>
+                <hr>
+                <li class="scroll-to-link" data-target="registrar-cliente">
+                    <a>Registrar Cliente</a>
+                </li>
+                <li class="scroll-to-link" data-target="registrar-orden">
+                    <a>Registrar Orden</a>
+                </li>
+
+                <hr>
+                <li class="scroll-to-link" data-target="diccionario-errores">
+                    <a>Diccionario de errores</a>
+                </li>
+                <hr>
+                <li class="scroll-to-link">
+                    <a class="nav-link" href="#"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                        style="text-decoration: none">Cerrar sesion
+                    </a>
+                    <form id="logout-form" action="/logout" method="POST" style="display: none;">
+                        @if (config('adminlte.logout_method'))
+                            {{ method_field(config('adminlte.logout_method')) }}
+                        @endif
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <div class="content-page">
+        <div class="content-code"></div>
+        <div class="content">
+            <div class="overflow-hidden content-section" id="content-introduccion">
+                <h2 id="introduccion">Introducción</h2>
+                <pre>
+                    <code class="json">
+Base URL
+
+https://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/api/
+
+                    </code>
+                        </pre>
+                <p>
+                    Api Jelou Mom, permite a las marcas una conexión directa con la plataforma de ventas, mediante una
+                    serie de pasos:
+                <ul>
+                    <li>
+                        Creación de la marca en el dashboard de Jelou:
+                        <ul>
+                            <li>
+                                <p class="introducction">
+                                    Definir si la marca requiere verificación de número de teléfono y solicitar
+                                    contraseña al usuario.
+                                </p>
+                            </li>
+                            <li>
+                                <p class="introducction">
+                                    Creación de token para la conexión con el api de la marca.
+                                </p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Agregar urls de los webhook o servicios para conectarse con la marca::
+                        <ul>
+                            <li>
+                                <p class="introducction">
+                                    Enviar mensaje con el código de verificación de usuario.
+                                </p>
+                            </li>
+                            <li>
+                                <p class="introducction">
+                                    Registrar un cliente.
+                                </p>
+                            </li>
+                            <li>
+                                <p class="introducction">
+                                    Registrar una venta.
+                                </p>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        Configuración de campos personalizados para la marca:
+                        <ul>
+                            <li>
+                                <p class="introducction">
+                                    La marca deberá entregar un documento donde debe especificar el nombre de los campos
+                                    y el tipo, de igual manera las validaciones respectivas.
+                                </p>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                </p>
+                <p>
+                    Todos los request contrendan en el encabezado Authorization: Bearer 2: EL TOKEN A UTILIZAR SE LE
+                    ASIGNARA A LA MARCA
+                </p>
+            </div>
+
+            <div class="overflow-hidden content-section" id="content-registrar-cliente">
+                <h2 id="registrar-cliente">Registrar Cliente</h2>
+                # Body
+                POST https://xxx/register-customer \
+                <pre><code class="json">
+{
+    "dni": "1234567890",
+    "name": "Pedro",
+    "last_name": "Perez",
+    "password": {
+        "value": "832ruhasd893",
+        "encrypted": true,
+        "type": "base64"
+    },
+    "email": {
+        "value": "pedroperez@gmail.com".
+        "verified": true,
+    },
+    "phone": {
+        "value": 19874894489,
+        "verified": true,
+    },
+    "profile_image": {
+        "type": "jpg",
+        "url": "https://www.$"%·"$&%/",
+        "timelife": 2342,
+        "date_create": "2022-05-03 10:00:00"
+        "timezone": "UTC"
+    },
+    "categories": [2,4],
+}
+                </code>
+            </pre>
+                <p>
+                    Petición POST:<br>
+                    <code class="higlighted">https://xxx/register-customer</code>
+                </p>
+                <br>
+                <pre>
+DATOS A ENVIAR
+<br>
+                <code class="json">
+        Respuesta Success:
+        {
+          "success": true,
+          "customer_id": 1,
+          "action": "CREATE"
+        }
+        {
+          "success": true,
+          "customer_id": 2,
+          "action": "UPDATE"
+        }
+        
+        Respuesta Error:
+        {
+          "success": false,
+          "error" : {
+            "code": 12,
+            "message" : "Campo edad invalido, ingresar un número"
+          }
+        }
+        
+                </code>
+            </pre>
+                <h4>DEFINICIÓN</h4>
+                <p>
+                    La marca debere especificar los campos que se requieren para el registro de usuario.
+                    Jelou por defecto tiene una lista de campos predeterminados, de igual manera, la marca puede añadir
+                    campos personalizados, solo debe proporcionar el nombre del campo y las validaciones. <br>
+                    Al enviarse los datos de un cliente, queda a criterio de la marca como verificar si el cliente ya se
+                    encuentra registrado en su base de datos y la actualización de los datos, lo exigido de parte de
+                    jelou es retornar el ID del usuario registrado, el cual posteriormente sera utilizado para procesar
+                    una venta.
+                </p>
+                <br>
+                <div class="row">
+                    <input type="text" name="url_brand">
+                </div>
+            </div>
+        </div>
         <script src="{{ mix('/js/app.min.js') }}"></script>
-    </body>
+</body>
+
 </html>
