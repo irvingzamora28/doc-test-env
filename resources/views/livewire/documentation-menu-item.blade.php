31c18d74e1d3e833f098d73c05017d8673cc8d28
Irving Zamora <irvingzamora28@gmail.com>
2022-06-02 02:27:28 -0600

    Agregar Item "Configuracion" al menu lateral

--- resources/views/livewire/documentation-menu-item.blade.php
+++ resources/views/livewire/documentation-menu-item.blade.php
@@ -1,6 +1,5 @@
 <span>
-    <li class="menu-item {{ $item['active'] ? 'active' : '' }}" wire:click="$emitUp('active', '{{$item['target']}}', '{{$item['active'] }}')"
-        data-target='{{ $item['target'] }}'>
+    <li class="menu-item {{ $item['active'] ? 'active' : '' }}" wire:click="$emitUp('active', '{{$item['target']}}', '{{$item['active'] }}')">
         <a>{{ $item['label'] }}</a>
     </li>
     <hr>
--- resources/views/livewire/documentation-menu.blade.php
+++ resources/views/livewire/documentation-menu.blade.php
@@ -1,7 +1,25 @@
 <div class="content-menu">
     <ul>
         @foreach ($menu as $item)
-            @livewire('documentation-menu-item', ['item' => $item[array_key_first($item)]], key($item[array_key_first($item)]['target']))
+            @livewire('documentation-menu-item', ['item' => $item[array_key_first($item)]], key($loop->index))
         @endforeach
+
+        <span>
+            <li class="menu-item">
+
+                <a>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                    </svg>
+                    Configuración
+                </a>
+            </li>
+            <hr>
+        </span>
+
+
+
     </ul>
 </div>
