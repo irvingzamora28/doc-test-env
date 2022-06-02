7b30a0cba0eff28ce5e2b7081a7f0fa3a15a3eb7
Irving Zamora <irvingzamora28@gmail.com>
2022-06-02 02:00:04 -0600

    Eliminar opcion "Cerrar sesion" del menu lateral

--- resources/views/livewire/documentation-menu.blade.php
+++ resources/views/livewire/documentation-menu.blade.php
@@ -3,18 +3,5 @@
         @foreach ($menu as $item)
             @livewire('documentation-menu-item', ['item' => $item[array_key_first($item)]], key($item[array_key_first($item)]['target']))
         @endforeach
-        <li class="menu-item">
-            <a class="nav-link" href="#"
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                style="text-decoration: none">Cerrar sesion
-            </a>
-            <form id="logout-form" action="/logout" method="POST" style="display: none;">
-                @if (config('adminlte.logout_method'))
-                    {{ method_field(config('adminlte.logout_method')) }}
-                @endif
-                {{ csrf_field() }}
-            </form>
-        </li>
-
     </ul>
 </div>
